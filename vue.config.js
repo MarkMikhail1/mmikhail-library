37e810680f0fa35a822e0ecd2eef29a8431c0364
MarkMikhail1 <markmikhail4@gmail.com>
2024-10-18 22:28:36 +1100

    Add github workflow

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from '@vue/cli-service';
 
-export default defineConfig({
-	transpileDependencies: true
-});
+
+module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+	  ? '/mmikhail-library/'
+	  : '/'
+  }
